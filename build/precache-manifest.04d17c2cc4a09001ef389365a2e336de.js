7096bd7f25493d43a6692f23b75830759959f5dc
sm92ha <sm92ha@gmail.com>
2019-02-18 06:31:49 +0330

    complete project

---
+++ build/precache-manifest.04d17c2cc4a09001ef389365a2e336de.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "405791253316a81f5dd97f8f182f37e9",
+    "url": "/static/media/anthony.40579125.jpg"
+  },
+  {
+    "revision": "bb90698d6efaa79283a1",
+    "url": "/static/css/main.508e488f.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "35218858a32caf218a9166ed4b5e2580",
+    "url": "/static/media/cc-bg-1.35218858.jpg"
+  },
+  {
+    "revision": "bb90698d6efaa79283a1",
+    "url": "/static/js/main.bb90698d.chunk.js"
+  },
+  {
+    "revision": "8221bc81454d40fd7900bda6d88770fd",
+    "url": "/static/media/staticmap.8221bc81.png"
+  },
+  {
+    "revision": "63b71bbaf4e6cb0602c5db268e66c7c3",
+    "url": "/static/media/nucleo-outline.63b71bba.woff2"
+  },
+  {
+    "revision": "1cf05903cbc4695ca3a1661843222429",
+    "url": "/static/media/nucleo-outline.1cf05903.woff"
+  },
+  {
+    "revision": "38f0cac08e7c2de4e6c99101301afdaf",
+    "url": "/static/media/nucleo-outline.38f0cac0.eot"
+  },
+  {
+    "revision": "17ef5c65f60c6b2ee86e76c844b46b27",
+    "url": "/static/media/nucleo-outline.17ef5c65.ttf"
+  },
+  {
+    "revision": "26d9c13968f26bb60fb4a52c505cab43",
+    "url": "/index.html"
+  }
+];
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import "./css/aos.css";
+import "./css/bootstrap.min.css";
+import "./styles/main.css";
+
+
+ReactDOM.render(<App />, document.getElementById('top'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
